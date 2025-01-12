1bd36a00fa8648b6e817e8d4745281fc752a22df
airxtech <anshul@airx.me>
2025-01-12 15:38:19 +0530

    Home page Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import styles from './page.module.css'
+import styles from './styles.module.css'
 
 export default function Home() {
   return (
